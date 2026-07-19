3fb944499e14bc7c43d351d847327728790bf03b
ilyabukhantsov <ilyabukhantsov5@gmail.com>
2026-07-19 15:58:58 +0300

    chore: add docker and db

--- cmd/aang/main.go
+++ cmd/aang/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	urlExample := "postgres://root:mysecretpassword@postgres-db:5432/my_database?sslmode=disable"
+	conn, err := pgx.Connect(context.Background(), urlExample)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("It works")
+
+	defer conn.Close(context.Background())
 }
