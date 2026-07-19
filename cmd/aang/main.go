2c1d4f913a7c73283cb918cd44b621467ad998e5
ilyabukhantsov <ilyabukhantsov5@gmail.com>
2026-07-19 17:08:55 +0300

    feat(db): create table

--- cmd/aang/main.go
+++ cmd/aang/main.go
@@ -3,14 +3,19 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/jackc/pgx/v5"
 )
 
 func main() {
+
+	ctx := context.Background()
+
 	urlExample := "postgres://root:mysecretpassword@postgres-db:5432/my_database?sslmode=disable"
-	conn, err := pgx.Connect(context.Background(), urlExample)
+	conn, err := pgx.Connect(ctx, urlExample)
+
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
@@ -19,4 +24,18 @@ func main() {
 	fmt.Println("It works")
 
 	defer conn.Close(context.Background())
+
+	const CreateDatabase = `
+		CREATE TABLE shorters (
+		shorterID INT PRIMARY KEY,
+    	original_url VARCHAR(150) NOT NULL,
+    	shorted_url VARCHAR(100) NOT NULL UNIQUE
+	);
+	`
+	_, err = conn.Exec(ctx, CreateDatabase)
+	if err != nil {
+		log.Fatalf("Failed to create shorter table: %v\n", err)
+	}
+	fmt.Println("Schema initialized: 'shorter' table created successfully.")
+
 }
